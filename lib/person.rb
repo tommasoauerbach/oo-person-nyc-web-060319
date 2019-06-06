6a5c4e6db9e9afb36a664d3b680aff8f41cd9974
tommaso auerbach <tsa302@nyu.edu>
2019-06-06 14:33:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,11 @@ class Person
 attr_accessor :bank_account
 attr_reader :name, :happiness, :hygiene
 
-def initialize(name, bank_account=25)
+def initialize(name, bank_account=25, happiness=8, hygiene=8)
   @name = name
   @bank_account = bank_account
+  @happiness = happiness
+  @hygiene = hygiene
 end
 
 end
