221476c7642059e4e799fad94bfa9f59e12f20ab
tommaso auerbach <tsa302@nyu.edu>
2019-06-06 14:03:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
+require 'pry'
+class Person
+attr_accessor :bank_account
+attr_reader :name, :happiness, :hygiene
+
+def get_paid(salary)
+self.bank_account += salary
+return "all about the benjamins"
+end
+
+binding.pry 
+end
+
 # your code goes here
